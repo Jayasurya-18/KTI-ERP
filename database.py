b706dc62e8c8a6c2a54e4ba32b10aa63ca2fc914
Jayasurya-18 <sjaya156120@gmail.com>
2026-07-30 15:00:28 +0530

    Database moved to Railway

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
+import os
 import mysql.connector
 
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="jayasurya*04",
-        database="knowledge_trading_erp"
-    )
+connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
